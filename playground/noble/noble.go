cc9be10804e0efb011666ec368ff6829f1f5c4b4
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-08-20 22:52:39 +0200

    add support for noble chain

---
+++ playground/noble/noble.go
@@ -0,0 +1,44 @@
+package noble
+
+import (
+	"github.com/hanchon/hanchond/playground/cosmosdaemon"
+	"github.com/hanchon/hanchond/playground/types"
+)
+
+var ChainInfo = types.NewChainInfo(
+	"noble",
+	"nobled",
+	"duke-",
+	"nobled",
+	"unoble",
+	"https://github.com/noble-assets/noble",
+	types.CosmosHDPath,
+	types.CosmosAlgo,
+)
+
+var _ cosmosdaemon.IDaemon = &Noble{}
+
+type Noble struct {
+	*cosmosdaemon.Daemon
+}
+
+func NewNoble(
+	moniker, version, homeDir, chainID, keyName string,
+	ports *types.Ports,
+) *Noble {
+	n := &Noble{
+		Daemon: cosmosdaemon.NewDaemon(
+			ChainInfo,
+			moniker,
+			version,
+			homeDir,
+			chainID,
+			keyName,
+			ports,
+		),
+	}
+
+	n.SetCustomConfig(n.UpdateNobleGenesisFile)
+
+	return n
+}
